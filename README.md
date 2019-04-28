cb27c7a13bea0b6ee10097dcfdf9922988226b4f
coxts2 <coxts2@student.op.ac.nz>
2019-04-28 18:59:53 +1200

    second part of the facial recognition

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # MachineLearning1-Face_Recognition
 https://github.com/ageitgey/face_recognition
+
+
+# Simple OpenCV Face Recognition in Python
+
+
+Face Recognition tutorial refference for http://thecodacus.com 
+
+### Prerequisites
+* Python 3.6.4
+* OpenCV 3.4.1 or opencv-contrib-python 4.0
+* Numpy
+* Pillow
+*pip install opencv-contrib-python
+
+### Installing
+
+* Create an empty Folder named **"dataSet"** in the same directory where the python scripts are 
+* Create an empty folder called **trainer** In same directory 
+
+## Running the tests
+
+* run the dataSetGenerator.py and enter a unique id to create face samples with your face
+* run trainer.py
+* run detector.py
+
+## Authors
+
+* **[Anirban Kar](http://thecodacus.com/author/admin/)**
---
+++ detector.py
@@ -0,0 +1,32 @@
+import cv2,os
+import numpy as np
+from PIL import Image 
+
+path = os.path.dirname(os.path.abspath(__file__))
+
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read(path+r'\trainer\trainer.yml')
+cascadePath = path+"\\Classifiers\\face.xml"
+faceCascade = cv2.CascadeClassifier(cascadePath);
+
+cam = cv2.VideoCapture(0)
+
+fontFace = cv2.FONT_HERSHEY_SIMPLEX  #Creates a font
+fontScale = 1
+fontColor = (255, 255, 255)
+
+while True:
+    ret, im =cam.read()
+    gray=cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+    faces=faceCascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5, minSize=(100, 100), flags=cv2.CASCADE_SCALE_IMAGE)
+    for(x,y,w,h) in faces:
+        nbr_predicted, conf = recognizer.predict(gray[y:y+h,x:x+w])
+        cv2.rectangle(im,(x-50,y-50),(x+w+50,y+h+50),(225,0,0),2)
+        if(nbr_predicted==7):
+             nbr_predicted='Obama'
+        elif(nbr_predicted==2):
+             nbr_predicted='Anirban'
+        cv2.putText(im,str(nbr_predicted)+"--"+str(conf), (x,y+h),fontFace, 1.1, (0,255,0)) #Draw the text
+        cv2.imshow('im',im)
+        cv2.waitKey(10)
+
