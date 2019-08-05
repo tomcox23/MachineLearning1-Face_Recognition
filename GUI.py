445a643841d8e8ccf4e8eb8ee31c807178f98c24
Thomas Cox (1000045206) <COXTS2@student.op.ac.nz>
2019-08-05 13:42:40 +1200

    removed set variable button

--- GUI.py
+++ GUI.py
@@ -16,43 +16,44 @@ dict={
 
 }
 
-name=""
-id_number=""
 
 def addFace():
-    id_number = txt_id.get()
-    path = os.path.dirname(os.path.abspath(__file__))
-    cam = cv2.VideoCapture(0)
-    detector=cv2.CascadeClassifier(path+r'\Classifiers\face.xml')
-    i=0
-    offset=50
-    #id_number needs to be a number ie a student id number
-    os.mkdir("dataSetFolders/"+id_number) # make directory dataSetFolders/
+	name = txt_name.get()
+	id_number = txt_id.get()
+	path = os.path.dirname(os.path.abspath(__file__))
+	cam = cv2.VideoCapture(0)
+	detector=cv2.CascadeClassifier(path+r'\Classifiers\face.xml')
+	i=0
+	offset=50
+	#id_number needs to be a number ie a student id number
+	os.mkdir("dataSetFolders/"+id_number) # make directory dataSetFolders/
 
-    while True:
-        ret, im=cam.read()  # Read the video frame
-        #take 21 images of a persons face and save images
-        if(im is not None):
-            gray=cv2.cvtColor(im,cv2.COLOR_BGR2GRAY) # Convert the captured frame into grayscale
-            faces=detector.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5, minSize=(100, 100), flags=cv2.CASCADE_SCALE_IMAGE) # Get all faces from the video frame
-            cv2.imshow('im',im)
-            
-            for(x,y,w,h) in faces:
-                i+=1
-                #cv2.imwrite("dataSet/face-"+id_number +'.'+ str(i) + ".jpg", gray[y-offset:y+h+offset,x-offset:x+w+offset]) #save images to dataset folder for the trainer script to access
-                cv2.imwrite("dataSetFolders/"+id_number+"/face-"+id_number +'.'+ str(i) + ".jpg", gray[y-offset:y+h+offset,x-offset:x+w+offset]) # save images to individual folders for each person, this is for image backups 
-                cv2.rectangle(im,(x-50,y-50),(x+w+50,y+h+50),(225,0,0),2)
-                cv2.imshow('im',im[y-offset:y+h+offset,x-offset:x+w+offset])
-                cv2.waitKey(100)
-            
-                if i>20:
-                    cam.release()
-                    cv2.destroyAllWindows()
-                break
+	while True:
+		ret, im=cam.read()  # Read the video frame
+		#take 21 images of a persons face and save images
+		if(im is not None):
+			gray=cv2.cvtColor(im,cv2.COLOR_BGR2GRAY) # Convert the captured frame into grayscale
+			faces=detector.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5, minSize=(100, 100), flags=cv2.CASCADE_SCALE_IMAGE) # Get all faces from the video frame
+			cv2.imshow('im',im)
+			
+			for(x,y,w,h) in faces:
+				i+=1
+				cv2.imwrite("dataSet/face-"+id_number +'.'+ str(i) + ".jpg", gray[y-offset:y+h+offset,x-offset:x+w+offset]) #save images to dataset folder for the trainer script to access
+				cv2.imwrite("dataSetFolders/"+id_number+"/face-"+id_number +'.'+ str(i) + ".jpg", gray[y-offset:y+h+offset,x-offset:x+w+offset]) # save images to individual folders for each person, this is for image backups 
+				cv2.rectangle(im,(x-50,y-50),(x+w+50,y+h+50),(225,0,0),2)
+				cv2.imshow('im',im[y-offset:y+h+offset,x-offset:x+w+offset])
+				cv2.waitKey(100)
+			
+				if i>20:
+					cam.release()
+					cv2.destroyAllWindows()
+				break
             
 #create Button + parameters, reference to def addFace
 btn_add_face = Button(window, text="Add Face", bg="black", fg="white",command=addFace)
 btn_add_face.grid(column=0, row=0, padx= 40, pady=50)
+
+
 
 def train():
     path = os.path.dirname(os.path.abspath(__file__))
@@ -178,14 +179,14 @@ def Exit():
 btn_exit = Button(window, text="Exit", bg="black", fg="white",command=Exit)
 btn_exit.grid(column=4, row=0, padx= 40)
 
-def setName():
-    name = txt_name.get()
-    id_number = txt_id.get()
-    dict[name]=id_number
+# def setName():
+    # name = txt_name.get()
+    # id_number = txt_id.get()
+    # dict[name]=id_number
 	
 #create Button + parameters, reference to def setName 
-btn_set_variables = Button(window, text="Set Variables", bg="black", fg="white",command=setName)
-btn_set_variables.grid(column=2, row=2)
+# btn_set_variables = Button(window, text="Set Variables", bg="black", fg="white",command=setName)
+# btn_set_variables.grid(column=2, row=2)
 
 #create set name text box
 lbl_name = Label(window, text="Name")
