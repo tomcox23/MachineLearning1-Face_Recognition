21fcdb465a30ff94870baeefe89bfc534e0f3b54
coxts2 <coxts2@student.op.ac.nz>
2019-09-23 00:00:35 +1200

    error checking

--- GUI.py
+++ GUI.py
@@ -1,6 +1,7 @@
 import sys
 import cv2
 import os
+import shutil
 import numpy as np
 import PIL.Image 
 from tkinter import *
@@ -16,7 +17,7 @@ user_dict = {
 
 }
 
-id = 1
+id = 0
 
 try:
     with open('users.json', 'r') as f:
@@ -25,60 +26,72 @@ try:
 except:
     print("An exception occurred")
 
-
-
-
+            
+            
+            
 def addFace(id):
     name = txt_name.get()
-    id_number = id
+    id_number = id # ERROR id_number is being set back to id
+    print("id number at start ",id_number)
     path = os.path.dirname(os.path.abspath(__file__))
     cam = cv2.VideoCapture(cv2.CAP_DSHOW)
     detector=cv2.CascadeClassifier(path+r'\Classifiers\face.xml')
     i=0
     offset=50
-    os.mkdir("dataSetFolders/"+str(id_number)) # make directory dataSetFolders/
-    user_dict[str(id_number)] = name
-    print(user_dict)
     
+    try:
+        os.mkdir("dataSetFolders/"+str(id_number)) # make directory dataSetFolders/
+        print("id number mkdir",id_number)
+    except:
+        print("dataSetFolder already exists error")
+        print("id number mkdir except ",id_number)
+        
+    user_dict[str(id_number)] = name
+        
     j = json.dumps(user_dict) # j is now a string containing the data from dict in the json format.
     with open('users.json', 'w') as f:
         f.write(j)
     
     print(user_dict)
 
-    while True:
-        
-        
-        ret, im=cam.read()  # Read the video frame
-        #take 21 images of a persons face and save images
-        if(im is not None):
-            gray=cv2.cvtColor(im,cv2.COLOR_BGR2GRAY) # Convert the captured frame into grayscale
-            faces=detector.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5, minSize=(100, 100), flags=cv2.CASCADE_SCALE_IMAGE) # Get all faces from the video frame
-            cv2.imshow('im',im)
+    try:
+        while True:
             
-            for(x,y,w,h) in faces:
-                i+=1
-                cv2.imwrite("dataSet/face-"+str(id_number) +'.'+ str(i) + ".jpg", gray[y-offset:y+h+offset,x-offset:x+w+offset]) #save images to dataset folder for the trainer script to access
-                cv2.imwrite("dataSetFolders/"+str(id_number)+"/face-"+str(id_number) +'.'+ str(i) + ".jpg", gray[y-offset:y+h+offset,x-offset:x+w+offset]) # save images to individual folders for each person, this is for image backups 
-                cv2.rectangle(im,(x-50,y-50),(x+w+50,y+h+50),(225,0,0),2)
-                cv2.imshow('im',im[y-offset:y+h+offset,x-offset:x+w+offset])
-                cv2.waitKey(100)
             
-                if i>20:
-                    cam.release()
-                    cv2.destroyWindow('im')
-                    id_number += 1
-                    #print (id_number)
-                break
+            ret, im=cam.read()  # Read the video frame
+            #take 21 images of a persons face and save images
+            if(im is not None):
+                gray=cv2.cvtColor(im,cv2.COLOR_BGR2GRAY) # Convert the captured frame into grayscale
+                faces=detector.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5, minSize=(100, 100), flags=cv2.CASCADE_SCALE_IMAGE) # Get all faces from the video frame
+                cv2.imshow('im',im)
                 
+                for(x,y,w,h) in faces:
+                    i+=1
+                    cv2.imwrite("dataSet/face-"+str(id_number) +'.'+ str(i) + ".jpg", gray[y-offset:y+h+offset,x-offset:x+w+offset]) #save images to dataset folder for the trainer script to access
+                    cv2.imwrite("dataSetFolders/"+str(id_number)+"/face-"+str(id_number) +'.'+ str(i) + ".jpg", gray[y-offset:y+h+offset,x-offset:x+w+offset]) # save images to individual folders for each person, this is for image backups 
+                    cv2.rectangle(im,(x-offset,y-offset),(x+w+offset,y+h+offset),(225,0,0),2)
+                    cv2.imshow('im',im[y-offset:y+h+offset,x-offset:x+w+offset])
+                    cv2.waitKey(100)
+                
+                    if i>20:
+                        cam.release()
+                        cv2.destroyWindow('im')
+                        id_number += 1
+                        print("Successful photo set id number ",id_number)
 
-        else:  
-            break
-       
+                    break
+                    
+
+            else:  
+                break
+    except:  
+        print("An error occurred. Try moving your face back from the camera")
+        shutil.rmtree("dataSetFolders/"+str(id_number)) # removes directory dataSetFolders/
+        del user_dict[str(id_number)]
+        print(user_dict)
+        print("Face too close id number ",id_number)
         
-
-	
-	
+        
 #create Button + parameters, reference to def addFace
 btn_add_face = Button(window, text="Add Face", bg="black", fg="white",command=lambda: addFace(id))
 btn_add_face.grid(column=0, row=0, padx= 40, pady=50)
